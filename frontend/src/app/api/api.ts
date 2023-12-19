7f0ef042a14c186d054de79c5c9afe2714af7fbd
timur <akimbaev.tim2002@gmail.com>
2023-12-19 15:31:23 +0600

    add adaptive styles to navbar, topbar

--- frontend/src/app/api/api.ts
+++ frontend/src/app/api/api.ts
@@ -5,7 +5,7 @@ import { error } from "console";
 const api = axios.create({
     baseURL: "http://localhost:8090/",
     withCredentials: true
-});
+}); 
 
 
 api.interceptors.request.use((config): any => {
@@ -13,7 +13,9 @@ api.interceptors.request.use((config): any => {
 
     config.headers['Authorization'] = `Bearer ${token}`
     config.headers['Content-Type'] = 'application/json'
-    config.headers['Access-Control-Allow-Origin'] = '*'
+    config.headers['Access-Control-Allow-Origin'] = 'http://localhost:8090/'
+    config.headers['Access-Control-Max-Age'] = '0'
+    config.headers['Access-Control-Allow-Methods'] = '*'
     return config;
 }, (error) => {
     return Promise.reject(error);
--- frontend/src/components/TopBar/index.tsx
+++ frontend/src/components/TopBar/index.tsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import './topBar.scss';
 import notification from '../../assets/images/Notification.svg';
 import avatar from '../../assets/images/Avatar.svg';
 
 function TopBar() {
+    const [nav, setNav] = useState<boolean>(false);
+    const dropdownRef = useRef<any>();
+
+    const links: any[] =  [
+        {'name': 'Dashboard', 'url': '/dashboard', },
+        {'name': 'Inventory', 'url': '/inventory',   },
+        {'name': 'Reports', 'url': '/reports',  },
+        {'name': 'Suppliers', 'url': '/suppliers',  },
+        {'name': 'Orders', 'url': '/orders',  },
+        {'name': 'Manage Store', 'url': '/manage-store',},
+    ]
+
+    const handleOutsideClick = (event: any) => {
+        if (dropdownRef.current && !dropdownRef.current.contains(event.target as Node)) {
+            setNav(false);
+        }
+    };
+
+
+    useEffect(() => {
+    // Добавляем обработчик события при монтировании компонента
+        document.addEventListener('click', handleOutsideClick);
+
+        // Убираем обработчик события при размонтировании компонента
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        };
+    }, []); //
+
   return (
     <div className='topbar'>
+        <div className='navbar_button'>
+            <button onClick={()=> setNav(!nav)} className='inventory_products_header_buttons_download'>
+                Navigation
+            </button>
+            {
+                nav ? <>
+                    <div  className='topbar_dropdown'>
+                        <ul className='topbar_dropdown_list'>
+                            {
+                                links.map((link) => (
+                                    <li key={link.url} className='topbar_dropdown_list_item'>
+                                        <a href={link.url}>
+                                            {link.name}
+                                        </a>
+                                    </li>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                </> : ''
+            }
+            
+        </div>
         <div className='topbar_search'>
             <input className="topbar_search_input" placeholder='Search product, supplier, order'/>
         </div>
